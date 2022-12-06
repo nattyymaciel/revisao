ccb97d6ab6e135630c32e8cbe0f6892a51bbd348
nattyymaciel <nattyymaciel@gmail.com>
2022-12-06 13:30:42 -0300

    nova 06/12

--- ex5novo.c
+++ ex5novo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "myLib.h"
 
 char *alocaString(int size){
     char *st;
@@ -15,7 +16,7 @@ int main(){
         printf("\nErro\n");
     }
     fscanf(arq, "%d", &size);
-    Imovel *vetImoveis =(Imovel *) malloc (size*seixeof(Imovel));
+    Imovel *vetImoveis =(Imovel *) malloc (size*sizeof(Imovel));
     //ler separador
     for(i=0; i<size; i++){
         fscanf(arq,"%s", lixo);
@@ -23,12 +24,17 @@ int main(){
         vetImoveis[i].tipoImovel=alocaString(size);
         fscanf(arq, "%s", &vetImoveis[i].finalidade);
         //repetir para demais dados do arquivo
+        fscanf(arq, "%s", &vetImoveis[i].bairro);
+        fscanf(arq, "%f", &vetImoveis[i].metragem);
+        fscanf(arq, "%d", &vetImoveis[i].dormitorios);
+        fscanf(arq, "%d", &vetImoveis[i].vagasGaragem);
+        fscanf(arq, "%f", &vetImoveis[i].valor);
     }
     fclose(arq);
     float valor;
-    Imovel aux= vendaMenorValor();
+    Imovel aux= vendaMenorValor(&vetImoveis, size, &valor);
     //tratar caso de n existir imovel com valor desejado
-    printf("Imovelde meor valor : %d , %f", aux.id, aux.tipoImovel...);
+    printf("Imovel de menor valor: %d , %f", aux.id, aux.tipoImovel);
 
     return 0;
 }
